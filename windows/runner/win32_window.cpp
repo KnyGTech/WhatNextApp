f89e232baafa8c7be7e807be6b9df0663a1d3eff
Knyihár Gábor <gabor.knyihar@aut.bme.hu>
2023-10-11 23:21:54 +0200

    Enable dark mode

--- windows/runner/win32_window.cpp
+++ windows/runner/win32_window.cpp
@@ -281,7 +281,8 @@ void Win32Window::UpdateTheme(HWND const window) {
                                &light_mode_size);
 
   if (result == ERROR_SUCCESS) {
-    BOOL enable_dark_mode = light_mode == 0;
+//    BOOL enable_dark_mode = light_mode == 0;
+    BOOL enable_dark_mode = 1;
     DwmSetWindowAttribute(window, DWMWA_USE_IMMERSIVE_DARK_MODE,
                           &enable_dark_mode, sizeof(enable_dark_mode));
   }
