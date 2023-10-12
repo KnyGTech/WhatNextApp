2ede2485d1de97713c0e1ad430635176e39cf923
Knyihár Gábor <gabor.knyihar@aut.bme.hu>
2023-10-12 14:50:06 +0200

    Make profile page responsive

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -23,9 +23,7 @@ class _ProfilePageState extends State<ProfilePage> {
             return _renderBody();
           } else {
             return Scaffold(
-                appBar: AppBar(
-                    title: Image.asset('assets/images/logo.png', height: 40)),
-                body: const Center(child: RefreshProgressIndicator()));
+                appBar: AppBar(title: Image.asset('assets/images/logo.png', height: 40)), body: const Center(child: RefreshProgressIndicator()));
           }
         });
   }
@@ -36,48 +34,53 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Scaffold(
             appBar: AppBar(
               title: Image.asset('assets/images/logo.png', height: 40),
-              actions: [
-                IconButton(onPressed: _refresh, icon: const Icon(Icons.refresh))
-              ],
-              bottom: const TabBar(isScrollable: true, tabs: [
-                Tab(child: Text('Sorozatok')),
-                Tab(child: Text('Aktivitás'))
-              ]),
+              actions: [IconButton(onPressed: _refresh, icon: const Icon(Icons.refresh))],
+              bottom: const TabBar(isScrollable: true, tabs: [Tab(child: Text('Sorozatok')), Tab(child: Text('Aktivitás'))]),
             ),
             body: Column(children: [
               _renderProfileCard(),
-              Expanded(
-                  child: TabBarView(
-                      children: [_renderShowsCard(), _renderActivityCard()]))
+              Expanded(child: TabBarView(children: [_renderShowsCard(), _renderActivityCard()]))
             ])));
   }
 
   Widget _renderProfileCard() {
+    if (ApplicationTheme.isMediumDevice(context)) {
+      return Card(
+        child: ListTile(
+          leading: _profile?.avatar != null
+              ? Image.network(_profile!.avatar,
+                  width: 50,
+                  loadingBuilder: ((context, child, loadingProgress) => loadingProgress == null ? child : Image.asset('assets/images/avatar.png')),
+                  errorBuilder: ((context, error, stackTrace) => Image.asset('assets/images/avatar.png')))
+              : Image.asset('assets/images/avatar.png'),
+          title: Text(_profile!.name, style: Theme.of(context).textTheme.titleLarge),
+          subtitle: Text(_profile!.email),
+          trailing: Text(_profile!.statistics, style: Theme.of(context).textTheme.titleLarge!.copyWith(color: ApplicationTheme.appColorBlue)),
+        ),
+      );
+    }
+
     return Card(
-      child: ListTile(
-        leading: _profile?.avatar != null
-            ? Image.network(_profile!.avatar,
-                width: 50,
-                loadingBuilder: ((context, child, loadingProgress) =>
-                    loadingProgress == null
-                        ? child
-                        : Image.asset('assets/images/avatar.png')),
-                errorBuilder: ((context, error, stackTrace) =>
-                    Image.asset('assets/images/avatar.png')))
-            : Image.asset('assets/images/avatar.png'),
-        title:
-            Text(_profile!.name, style: Theme.of(context).textTheme.titleLarge),
-        subtitle: Text(_profile!.email),
-        trailing: Text(_profile!.statistics,
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge!
-                .copyWith(color: ApplicationTheme.appColorBlue)),
-      ),
+      child: Column(children: [
+        ListTile(
+          leading: _profile?.avatar != null
+              ? Image.network(_profile!.avatar,
+                  width: 50,
+                  loadingBuilder: ((context, child, loadingProgress) => loadingProgress == null ? child : Image.asset('assets/images/avatar.png')),
+                  errorBuilder: ((context, error, stackTrace) => Image.asset('assets/images/avatar.png')))
+              : Image.asset('assets/images/avatar.png'),
+          title: Text(_profile!.name, style: Theme.of(context).textTheme.titleLarge),
+          subtitle: Text(_profile!.email),
+        ),
+        Text(_profile!.statistics, style: Theme.of(context).textTheme.titleLarge!.copyWith(color: ApplicationTheme.appColorBlue)),
+        const SizedBox(height: 10)
+      ]),
     );
   }
 
   Widget _renderShowsCard() {
+    var crossAxisCount = (MediaQuery.of(context).size.width / 130).round();
+
     return Card(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -88,10 +91,8 @@ class _ProfilePageState extends State<ProfilePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("Sorozatok",
-                      style: Theme.of(context).textTheme.titleLarge),
-                  Text('${_profile!.shows.length} db',
-                      style: Theme.of(context).textTheme.titleLarge),
+                  Text("Sorozatok", style: Theme.of(context).textTheme.titleLarge),
+                  Text('${_profile!.shows.length} db', style: Theme.of(context).textTheme.titleLarge),
                 ],
               )),
           Expanded(
@@ -99,11 +100,8 @@ class _ProfilePageState extends State<ProfilePage> {
                   onRefresh: _refresh,
                   child: GridView.builder(
                     padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 15.0),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            childAspectRatio: 2.45, crossAxisCount: 3),
-                    itemBuilder: (context, index) =>
-                        Card(child: ImageBanner(_profile!.shows[index])),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(childAspectRatio: 2.45, crossAxisCount: crossAxisCount),
+                    itemBuilder: (context, index) => Card(elevation: 0,child: ImageBanner(_profile!.shows[index])),
                     itemCount: _profile!.shows.length,
                   ))),
         ],
@@ -117,10 +115,7 @@ class _ProfilePageState extends State<ProfilePage> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text("Utoljára megtekintett epizódok",
-                  style: Theme.of(context).textTheme.titleLarge)),
+          Padding(padding: const EdgeInsets.all(15.0), child: Text("Utoljára megtekintett epizódok", style: Theme.of(context).textTheme.titleLarge)),
           Expanded(
               child: RefreshIndicator(
                   onRefresh: _refresh,
@@ -128,9 +123,8 @@ class _ProfilePageState extends State<ProfilePage> {
                     padding: EdgeInsets.zero,
                     itemBuilder: (context, index) => ListTile(
                       dense: true,
-                      leading: ImageBanner(_profile!.activities[index].banner),
-                      title: Text(_profile!.activities[index].show,
-                          style: Theme.of(context).textTheme.titleLarge),
+                      leading: ApplicationTheme.isSmallDevice(context) ? ImageBanner(_profile!.activities[index].banner, size: ApplicationTheme.isMediumDevice(context) ? null : 65) : null,
+                      title: Text(_profile!.activities[index].show, style: Theme.of(context).textTheme.titleLarge),
                       subtitle: Text(_profile!.activities[index].episode),
                       trailing: Text(_profile!.activities[index].time),
                     ),
